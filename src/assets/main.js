ca14810bcd07a830fec32ec23f95d118cd0d88a8
billbo85 <b.bregy@gmail.com>
2017-02-20 11:36:48 +0100

    add my completed courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: "https://www.codeschool.com/users/billbo85.json",
+    dataType: 'jsonp',
+    success: function(response) {
+        var courses = response.courses.completed;
+
+        courses.forEach(function(course){
+            $badge = $('#badges');
+
+            $div = $('<div>', {'class': 'course'}).appendTo($badge);
+            $('<h3>', {'text': course.title}).appendTo($div);
+            $('<img>').attr('src', course.badge).appendTo($div);
+            $('<a>', { 'class':'btn btn-primary' , 'href': course.url, 'target': '_blank', 'text': 'See course'}).appendTo($div);
+        });
+
+
+    }
+  });
 
 });
